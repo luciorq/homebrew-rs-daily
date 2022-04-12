84b5a08f0fd4aae18ba8b19aa63f918b4d8f985d
luciorq <luciorqueiroz@gmail.com>
2022-04-11 20:35:28 -0400

    chore: update formula for 2022.06.0-daily-288

--- Casks/rstudio-daily-electron.rb
+++ Casks/rstudio-daily-electron.rb
@@ -1,8 +1,8 @@
 cask "rstudio-daily-electron" do
-  version "2022.06.0-daily+284"
-  sha256 "c7b08226a6cfd86deddb58f6915ebc0604aae229cb4e7cd0f3d31875f77c74c6"
+  version "2022.06.0-daily+288"
+  sha256 "5635b6b812b7073af87e6f59d50ad47814a8245d28a7ca74c0aa2a5e52422b2b"
 
-  url "https://s3.amazonaws.com/rstudio-ide-build/electron/macos/RStudio-2022.06.0-daily-284.dmg"
+  url "https://s3.amazonaws.com/rstudio-ide-build/electron/macos/RStudio-2022.06.0-daily-288.dmg"
   name "RStudio"
   homepage "https://dailies.rstudio.com/rstudio/spotted-wakerobin/electron/macos/"
 
