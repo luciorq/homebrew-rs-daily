52d430e9192b4fe01722ae4b3a0d13f0e743fab7
luciorq <luciorqueiroz@gmail.com>
2022-04-28 13:39:42 -0400

    chore: version upgrade - 2022.06.0-daily+339

--- Casks/rstudio-daily-electron.rb
+++ Casks/rstudio-daily-electron.rb
@@ -1,8 +1,8 @@
 cask "rstudio-daily-electron" do
-  version ""
-  sha256 ""
+  version "2022.06.0-daily+339"
+  sha256 "daac78e70fdea994f18326b6916be1b2b413b93dac267c2a446f0562964a0232"
 
-  url ""
+  url "https://s3.amazonaws.com/rstudio-ide-build/electron/macos/RStudio-2022.06.0-daily-339.dmg"
   name "RStudio"
   homepage "https://dailies.rstudio.com/rstudio/spotted-wakerobin/electron/macos/"
 
--- src/build_formulas.sh
+++ src/build_formulas.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 function build_formulas () {
-  local mkdir_bin cat_bin;
+  local mkdir_bin cat_bin sed_bin;
   local jq_bin;
   local json_var;
   local rs_daily_url rs_daily_hash rs_version;
@@ -9,16 +9,37 @@ function build_formulas () {
   formula_name='rstudio-daily-electron';
   template_path="templates/${formula_name}.rb";
   jq_bin="$(builtin command -p which 'jq')";
+  sed_bin="$(builtin command -p which 'sed')";
   cat_bin="$(builtin command -p which 'cat')";
   mkdir_bin="$(builtin command -p which 'mkdir')";
-  json_var="$(curl -L -s -S -f https://dailies.rstudio.com/rstudio/latest/index.json)"
-  rs_daily_url=$(builtin echo "${json_var}" | "${jq_bin}" '.products.electron.platforms.macos.link | values' | sed 's|\"||g');
-  rs_daily_hash=$(builtin echo "${json_var}" | "${jq_bin}" '.products.electron.platforms.macos.sha256 | values' | sed 's|\"||g');
-  rs_version=$(builtin echo "${json_var}" | "${jq_bin}" '.products.electron.platforms.macos.version | values' | sed 's|\"||g');
-  "${mkdir_bin}" -p Casks
+  json_var="$(
+    curl -L -s -S -f https://dailies.rstudio.com/rstudio/latest/index.json
+  )";
+  rs_daily_url=$(
+    builtin echo ${json_var} \
+      | "${sed_bin}" 's|, }| }|g' \
+      | "${jq_bin}" '.products.electron.platforms.macos.link | values' \
+      | "${sed_bin}" 's|\"||g'
+  );
+  rs_daily_hash=$(
+    builtin echo ${json_var} \
+      | "${sed_bin}" 's|, }| }|g' \
+      | "${jq_bin}" '.products.electron.platforms.macos.sha256 | values' \
+      | "${sed_bin}" 's|\"||g'
+  );
+  rs_version=$(
+    builtin echo ${json_var} \
+      | "${sed_bin}" 's|, }| }|g' \
+      | "${jq_bin}" '.products.electron.platforms.macos.version | values' \
+      | "${sed_bin}" 's|\"||g'
+  );
+  if [[ ! -d Casks ]]; then
+    "${mkdir_bin}" -p Casks;
+  fi
   "${cat_bin}" "${template_path}" \
-    | sed "s|<<<RS_VERSION>>>|${rs_version}|g" \
-    | sed "s|<<<RS_URL>>>|${rs_daily_url}|g" \
-    | sed "s|<<<RS_SHA>>>|${rs_daily_hash}|g" > "Casks/${formula_name}.rb"
+    | "${sed_bin}" "s|<<<RS_VERSION>>>|${rs_version}|g" \
+    | "${sed_bin}" "s|<<<RS_URL>>>|${rs_daily_url}|g" \
+    | "${sed_bin}" "s|<<<RS_SHA>>>|${rs_daily_hash}|g" \
+    > "Casks/${formula_name}.rb";
   builtin echo -ne "${rs_version}";
 }
