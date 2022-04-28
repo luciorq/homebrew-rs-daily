a9911e17c8339c251c974782c456da6d64e1829c
luciorq <luciorqueiroz@gmail.com>
2022-04-28 12:59:26 -0400

    chore: version upgrade -

--- Casks/rstudio-daily-electron.rb
+++ Casks/rstudio-daily-electron.rb
@@ -1,8 +1,8 @@
 cask "rstudio-daily-electron" do
-  version "2022.06.0-daily+322"
-  sha256 "ab09719d8d551a672206c1493d5f73ea4f4a30877468e9c99994ad7a58102122"
+  version ""
+  sha256 ""
 
-  url "https://s3.amazonaws.com/rstudio-ide-build/electron/macos/RStudio-2022.06.0-daily-322.dmg"
+  url ""
   name "RStudio"
   homepage "https://dailies.rstudio.com/rstudio/spotted-wakerobin/electron/macos/"
 
