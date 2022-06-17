bf97ccbbb759435b473c3544f4456b3be3f99366
luciorq <luciorqueiroz@gmail.com>
2022-06-17 15:57:00 -0400

    chore: version upgrade - 2022.06.0-daily+531

--- Casks/rstudio-daily-electron.rb
+++ Casks/rstudio-daily-electron.rb
@@ -1,8 +1,8 @@
 cask "rstudio-daily-electron" do
-  version "2022.06.0-daily+439"
-  sha256 "cb26c3f528b5f907a60b776b726543f0731a60db16d9f0bfe7f654f3d18dff2b"
+  version "2022.06.0-daily+531"
+  sha256 "70962d0ca75ba503a33ee96b70347d3277802bc4ad465c5914a59294c746c9f9"
 
-  url "https://s3.amazonaws.com/rstudio-ide-build/electron/macos/RStudio-2022.06.0-daily-439.dmg"
+  url "https://s3.amazonaws.com/rstudio-ide-build/electron/macos/RStudio-2022.06.0-daily-531.dmg"
   name "RStudio"
   homepage "https://dailies.rstudio.com/rstudio/spotted-wakerobin/electron/macos/"
 
--- justfile
+++ justfile
@@ -2,7 +2,7 @@ build:
     #!/usr/bin/env bash
     set -euxo pipefail;
     source src/build_formulas.sh;
-    rs_version=$(build_formulas);
+    rs_version="$(build_formulas)";
     git add --all;
     git commit -m "chore: version upgrade - ${rs_version}";
     git push;
