281b166994129c4acf9a4a66331ab7035f8f4e82
luciorq <luciorqueiroz@gmail.com>
2022-07-25 12:26:18 -0400

    fix: hash fix

--- Casks/rstudio-daily-electron.rb
+++ Casks/rstudio-daily-electron.rb
@@ -1,6 +1,6 @@
 cask "rstudio-daily-electron" do
   version "2022.11.0-daily+78"
-  sha256 "4b1ab10ec1eddb3b031b72c57d449ef1c71c122f23c16ecc19736a278ebaab7d"
+  sha256 "fe4c6def6e3d33c88b4a7988241fd388897b9adcec68034475b74375dea62894"
 
   url "https://s3.amazonaws.com/rstudio-ide-build/electron/macos/RStudio-2022.11.0-daily-78.dmg"
   name "RStudio"
