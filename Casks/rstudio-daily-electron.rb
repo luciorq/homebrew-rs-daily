8b18907d05b66b28ac362af6e3e61a65a7b401a5
luciorq <luciorqueiroz@gmail.com>
2023-10-04 17:31:05 -0400

    chore: version upgrade - 2023.12.0-daily+119

--- Casks/rstudio-daily-electron.rb
+++ Casks/rstudio-daily-electron.rb
@@ -1,8 +1,8 @@
 cask "rstudio-daily-electron" do
-  version "2023.09.0+448"
-  sha256 "032753fa67507e0ca0b5f4ba5c116ee58f47cd1345bd4da643862f227715e59d"
+  version "2023.12.0-daily+119"
+  sha256 "3452bc96c3d584b4a34ee3a78c1ede2952e55e74ad39349944719c85c43ff444"
 
-  url "https://s3.amazonaws.com/rstudio-ide-build/electron/macos/RStudio-2023.09.0-448.dmg"
+  url "https://s3.amazonaws.com/rstudio-ide-build/electron/macos/RStudio-2023.12.0-daily-119.dmg"
   name "RStudio"
   homepage "https://dailies.rstudio.com/rstudio/spotted-wakerobin/electron/macos/"
 
